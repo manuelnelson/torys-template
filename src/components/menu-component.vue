853ae2bc4b4ae2ab8ea8c777cf52a3cd282ebf9e
manuelnelson <elnels@gmail.com>
2018-10-19 17:40:04 -0400

    industries shown

--- src/components/menu-component.vue
+++ src/components/menu-component.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="map-menu" @click="toggleMenu" :class="{'open':menuOpen}">
-      <div class="map-menu__menu"></div>
+      <div class="map-menu__menu">
+        <div class="service-list">
+          <div class="service-item" v-for="(industry,ndx) in industries" :key="ndx">{{industry.Name}}</div>
+        </div>
+      </div>
       <div class="map-menu__collapsible"></div>
+      <div class="map-menu__project">
+
+      </div>
   </div>
 </template>
 <script>
@@ -9,11 +16,14 @@ export default {
   data() {
     return {
       menuOpen: false,
+      industries: window.__INTIALSTATE__.industries,
+      regions: window.__INTIALSTATE__.regions
     }
   },
   components: {
   },
   mounted() {
+    console.log(this.industries)
   },
   methods: {
     toggleMenu() {
